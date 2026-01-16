5dc488e41cbdec7b2ec94744a6f1ebee7cda1e94
Josh Richau <jrichau@gmail.com>
2026-01-16 11:47:19 -0700

    improved type exports for codelists too, bump release again!

--- src/codelists.ts
+++ src/codelists.ts
@@ -3,6 +3,26 @@
  * @packageDocumentation
  */
 
-import codelistIndex from "./codelist_index.json" with { type: "json" }
+import codelistIndexJson from "./codelist_index.json" with { type: "json" }
 
+export interface CodelistValue {
+  code: string
+  term: string
+  synonyms?: string | null
+  definition: string
+  [key: string]: any
+}
+
+export interface CodelistEntry {
+  codelist_code: string
+  extensible: boolean
+  values: CodelistValue[]
+  [key: string]: any
+}
+
+export interface CodelistIndex {
+  [key: string]: CodelistEntry
+}
+
+const codelistIndex: CodelistIndex = codelistIndexJson as CodelistIndex
 export default codelistIndex
